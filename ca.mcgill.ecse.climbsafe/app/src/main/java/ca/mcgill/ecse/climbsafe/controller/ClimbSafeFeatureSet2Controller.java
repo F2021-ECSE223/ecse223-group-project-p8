c296ece5f98b569e7b53980171ee3266dc9d0f06
ke.yan@mail.mcgill.ca <ke.yan@mail.mcgill.ca>
2021-10-16 01:41:20 -0400

    Started member controller

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet2Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet2Controller.java
@@ -3,10 +3,32 @@ package ca.mcgill.ecse.climbsafe.controller;
 import java.util.List;
 
 public class ClimbSafeFeatureSet2Controller {
+	
+	//check if it should it be static
+	//use this as...
+	private static ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
 
   public static void registerMember(String email, String password, String name,
       String emergencyContact, int nrWeeks, boolean guideRequired, boolean hotelRequired,
-      List<String> itemNames, List<Integer> itemQuantities) throws InvalidInputException {}
+      List<String> itemNames, List<Integer> itemQuantities) throws InvalidInputException {
+	  
+	  var error = "";
+	  
+	  try {
+		  climbSafe.addMember(email, password, name, emergencyContact, nrWeeks, guideRequired, hotelRequired);
+		  //what to do with list?
+	  } catch (InvalidInputException e) {
+		  //is this the right error? do I throw?
+		  //throw new InvalidInputException(e.getMessage());
+		  error += e.getMessage();
+	  }
+	  
+	  try {
+		  
+	  } catch () {
+		  
+	  }
+  }
 
   public static void updateMember(String email, String newPassword, String newName,
       String newEmergencyContact, int newNrWeeks, boolean newGuideRequired,
