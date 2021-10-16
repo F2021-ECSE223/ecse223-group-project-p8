ad758184b3817c0a1489f78a1e45b2ab77e5d4eb
Mihail C <68878155+MihailOfficial@users.noreply.github.com>
2021-10-15 21:36:03 -0400

    almost done the controller, but not finished

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet6Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet6Controller.java
@@ -1,14 +1,60 @@
 package ca.mcgill.ecse.climbsafe.controller;
 
 import java.util.List;
+import java.util.Map;
 
 public class ClimbSafeFeatureSet6Controller {
 
-  public static void deleteEquipment(String name) throws InvalidInputException {}
+  public static void deleteEquipment(String name) throws InvalidInputException {
+	  
+	
+	  BookableItem equipmentToDelete = (ClimbSafeApplication.getClimbSafe()).getWithName(name);
+	 
+	  if(equipmentToDelete!=null) {
+		 
+		  equipmentToDelete.delete();
+	  }
+  }
 
   // this method does not need to be implemented by a team with five team members
-  public static void deleteEquipmentBundle(String name) {}
+  public static void deleteEquipmentBundle(String name) {
+	  BookableItem equipmentBundleToDelete = (ClimbSafeApplication.getClimbSafe()).getWithName(name);
+		 
+	  if(equipmentBundleToDelete!=null) {
+		 
+		  equipmentBundleToDelete.delete();
+	  }
+  }
 
-  public static List<TOAssignment> getAssignments() {}
+  public static List<TOAssignment> getAssignments() {
+	  
+	  // NOT DONE //  NOT DONE //  NOT DONE //  NOT DONE //  NOT DONE
+	  
+	  List<Assignment> temp = (ClimbSafeApplication.getClimbSafe()).getAssignments();
+	  List<TOAssignment> resultAssignment;
+	  for(Assignment current : resultAssignment) {
+		  String aMemberEmail = current.getMember().getEmail();
+		  String aMemberName = current.getMember().getName();
+		  String aGuideEmail = current.getGuide().getEmail();
+		  String aGuideName = current.getGuide().getName();
+		  String aHotelName = current.getHotel().getName();
+		  int aStartWeek = current.getStartWeek();
+		  int aEndWeek = current.getEndWeek();
+		  int aTotalCostForGuide = (ClimbSafeApplication.getClimbSafe()).getPriceOfGuidePerWeek();
+		  //int aTotalCostForEquipment = ;
+	
+		  //  NOT DONE //  NOT DONE //  NOT DONE //  NOT DONE //  NOT DONE //  NOT DONE //  NOT DONE
+		  
+		  
+		  resultAssignment.add(new ToAssignment(aMemberEmail,aMemberName, aGuideEmail, aGuideName, aHotelName, aStartWeek, aEndWeek, aTotalCostForGuide, aTotalCostForEquipment));
+		  
+		  var name = equipment.get("name");
+		  var weight = equipment.get("weight");
+		  var pricePerWeek = equipment.get("pricePerWeek");
+		  new Equipment(name, Integer.parseInt(weight), Integer.parseInt(pricePerWeek), climbSafe);
+	  }
+	  
+	  
+  }
 
 }
