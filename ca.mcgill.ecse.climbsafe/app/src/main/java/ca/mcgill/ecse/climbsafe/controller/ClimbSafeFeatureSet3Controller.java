484def504e216c29b9d4cb03274cc243c1a2290c
selinagao <selinagao1004@gmail.com>
2021-10-20 12:50:14 -0400

    first commit try

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet3Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet3Controller.java
@@ -3,7 +3,13 @@ package ca.mcgill.ecse.climbsafe.controller;
 public class ClimbSafeFeatureSet3Controller {
 
   public static void registerGuide(String email, String password, String name,
-      String emergencyContact) throws InvalidInputException {}
+      String emergencyContact, int priceOfGuidePerWeek) 
+    		  throws InvalidInputException {
+	  var error = "";
+	  try {
+		  
+	  }
+  }
 
   public static void updateGuide(String email, String newPassword, String newName,
       String newEmergencyContact) throws InvalidInputException {}
