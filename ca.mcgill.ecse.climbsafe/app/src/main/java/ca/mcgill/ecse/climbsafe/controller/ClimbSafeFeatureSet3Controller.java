54d3b2183dc2cd97fcf62ce4ef7c985cd83a815d
J03YL1u <lucas.liuliongwah@mail.mcgill.ca>
2021-10-23 18:40:58 -0400

    set 2 & 3

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet3Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet3Controller.java
@@ -2,24 +2,106 @@ package ca.mcgill.ecse.climbsafe.controller;
 
 import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
 import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
+<<<<<<< HEAD
+=======
+import ca.mcgill.ecse.climbsafe.model.Guide;
+import ca.mcgill.ecse.climbsafe.model.Member;
+>>>>>>> branch 'main' of https://github.com/F2021-ECSE223/ecse223-group-project-p8
 
 public class ClimbSafeFeatureSet3Controller {
 	 private static ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
+	 
+	 
+	 // idk how to do javadoc 
+	 /**
+	  * 
+	  * @param email Email of Guide that gets entered and that is registered in the climbSafe system
+	  * @param password Password of Guide that gets entered and that is registered in the climbSafe system
+	  * @param name First name of Guide that is entered and registered in the climbSafe system
+	  * @param emergencyContact Guide enters an emergency contact's phone number and this is registered in the climbSafe system
+	  * @throws InvalidInputException for inputs when errors occur 
+	  */
 
   public static void registerGuide(String email, String password, String name,
       String emergencyContact) throws InvalidInputException {
 	 
-	  
 	  var error = "";
-	  try {
-		  climbSafe.addGuide(email, password, name, emergencyContact);
+	  
+	  if(email.isEmpty()) {
+		  error="Email cannot be empty";
+		  throw new InvalidInputException(error);
 	  }
-	  catch (RuntimeException e){
-		  
-		  error += e.getMessage();
-		  
-		  throw new InvalidInputException(error); 
+	  
+	  if(password.isEmpty()) {
+		  error="Password cannot be empty";
+		  throw new InvalidInputException(error);
 	  }
+	  
+	  if(name.isEmpty()) {
+		  error="Name cannot be empty";
+		  throw new InvalidInputException(error);
+	  }
+	  
+	  if(emergencyContact.isEmpty()) {
+		  error="Emergency contact cannot be empty";
+		  throw new InvalidInputException(error);
+	  }
+	  
+	  String adminEmail="admin@nmc.nt";
+	  if(email.equals(adminEmail)) {
+		  error="Email cannot be admin@nmc.nt";
+		  throw new InvalidInputException(error);
+	  }
+	 
+	  Guide existingGuide = Utility.findGuide(email);
+	  if(existingGuide != null) {
+		  error="Email already linked to a guide account";
+		  throw new InvalidInputException(error);
+	  }
+	  
+	  Member existingMember = Utility.findMember(email);
+	  if(existingMember != null) {
+		  error="Email already linked to a member account";
+		  throw new InvalidInputException(error);
+	  }
+	  
+	  if(email.contains(" ")) {
+		  error="Email must not contain any spaces";
+		  throw new InvalidInputException(error);
+	  }
+	  
+	  if(Utility.emailInvalidSyntax(email) == true) {
+		  error="Invalid email";
+		  throw new InvalidInputException(error);
+	  }
+	  
+	  if(Utility.emailMissingSubstring(email) == true) {
+		  error="Invalid Email";
+		  throw new InvalidInputException(error);
+	  }
+	  
+	  if(Utility.missingEmailDeclaration(email) == true) {
+		  error="Invalid Email";
+		  throw new InvalidInputException(error);
+	  }
+	  
+	  if(!email.contains(name.toLowerCase())) {
+		  error="Invalid Email";
+		  throw new InvalidInputException(error);
+	  }
+	  
+	 if(Utility.wrongEmailSyntax(email) == true) {
+		 error="Invalid Email";
+		 throw new InvalidInputException(error);
+	 }
+	  
+	 try {
+		 climbSafe.addGuide(email, password, name, emergencyContact); 
+	 }
+	 catch (RuntimeException e){
+		 error += e.getMessage();
+		 throw new InvalidInputException(error); 
+	 }
   }
 
   public static void updateGuide(String email, String newPassword, String newName,
