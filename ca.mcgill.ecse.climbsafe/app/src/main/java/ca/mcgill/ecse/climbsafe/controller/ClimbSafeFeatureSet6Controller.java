602474be293da9584b09e0c3cf55dcb62e2c3791
Mihail C <calitoiumihail@gmail.com>
2021-10-25 01:34:57 -0400

    changed some variable names

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet6Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet6Controller.java
@@ -1,9 +1,7 @@
 package ca.mcgill.ecse.climbsafe.controller;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
 import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
 import ca.mcgill.ecse.climbsafe.model.Assignment;
 import ca.mcgill.ecse.climbsafe.model.BookableItem;
@@ -13,8 +11,8 @@ import ca.mcgill.ecse.climbsafe.model.Equipment;
 import ca.mcgill.ecse.climbsafe.model.EquipmentBundle;
 import ca.mcgill.ecse.climbsafe.model.Member;
 
-
 //@Mihail Calitoiu
+
 public class ClimbSafeFeatureSet6Controller {
 
     public static void deleteEquipment(String name) throws InvalidInputException {
@@ -30,33 +28,33 @@ public class ClimbSafeFeatureSet6Controller {
         }
 
         if (equipment != null) {
-
             equipment.delete();
         }
     }
 
     public static void deleteEquipmentBundle(String name) {
-        EquipmentBundle equipmentBundle = null;
+        
+    	EquipmentBundle equipmentBundle = null;
         List < EquipmentBundle > equipmentBundleList = (ClimbSafeApplication.getClimbSafe()).getBundles();
 
         for (EquipmentBundle temp: equipmentBundleList) {
-            if (temp.getName().equals(name)) {
-                equipmentBundle = temp;
-                break;
+        	if (temp.getName().equals(name)) {
+        		equipmentBundle = temp;
+                break; 
             }
         }
 
         if (equipmentBundle != null) {
-
             equipmentBundle.delete();
         }
+        
     }
 
     public static List < TOAssignment > getAssignments() {
 
 
         List < Assignment > temp = (ClimbSafeApplication.getClimbSafe()).getAssignments();
-        List < TOAssignment > resultAssignment = new ArrayList < > ();
+        List < TOAssignment > assignment2ToAssignment = new ArrayList <>();
 
         for (Assignment current: temp) {
 
@@ -65,6 +63,7 @@ public class ClimbSafeFeatureSet6Controller {
             String aGuideEmail = current.getGuide().getEmail();
             String aGuideName = current.getGuide().getName();
             String aHotelName = current.getHotel().getName();
+            
             int aStartWeek = current.getStartWeek();
             int aEndWeek = current.getEndWeek();
             int stayedWeeks = aEndWeek - aStartWeek;
@@ -76,28 +75,34 @@ public class ClimbSafeFeatureSet6Controller {
             for (BookedItem currentBooked: membersItems) {
 
                 if (currentBooked.getItem().getClass().equals("EquipmentBundle")) {
-                    EquipmentBundle specificItem = (EquipmentBundle) currentBooked.getItem();
+                   
+                	EquipmentBundle specificItem = (EquipmentBundle) currentBooked.getItem();
                     List < BundleItem > storedEquipment = specificItem.getBundleItems();
                     int costForBundle = 0;
+                    
                     for (BundleItem currentEqi: storedEquipment) {
-                        int price = currentEqi.getEquipment().getPricePerWeek() * stayedWeeks;
-                        costForBundle += price;
+                        
+                    	int price = currentEqi.getEquipment().getPricePerWeek() * stayedWeeks;
+                    	costForBundle += price;
+                    	
                     }
+                    
                     costForBundle = costForBundle * ((100 - specificItem.getDiscount()) / 100);
                     TotalCostForEquipment += costForBundle;
 
                 } else if (currentBooked.getItem().getClass().equals("Equipment")) {
+                	
                     Equipment specificItem = (Equipment) currentBooked.getItem();
                     TotalCostForEquipment += specificItem.getPricePerWeek() * stayedWeeks * currentBooked.getQuantity();
+                    
                 }
             }
 
-            resultAssignment.add(new TOAssignment(aMemberEmail, aMemberName, aGuideEmail, aGuideName, aHotelName, aStartWeek, aEndWeek, TotalCostForGuide, TotalCostForGuide));
-
+            assignment2ToAssignment.add(new TOAssignment(aMemberEmail, aMemberName, aGuideEmail, aGuideName, aHotelName, aStartWeek, aEndWeek, TotalCostForGuide, TotalCostForGuide));
 
         }
 
-        return resultAssignment;
+        return assignment2ToAssignment;
     }
 
 
