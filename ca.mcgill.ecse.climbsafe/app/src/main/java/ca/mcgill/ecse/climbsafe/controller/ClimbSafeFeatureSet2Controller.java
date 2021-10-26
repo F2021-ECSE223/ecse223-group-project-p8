aa03d5adac2fadd973af696b8d8589ccc48b5d0c
ke-yan <ke.yan@mail.mcgill.ca>
2021-10-26 00:30:02 -0400

    Added conditions for update member

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet2Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet2Controller.java
@@ -153,8 +153,61 @@ public class ClimbSafeFeatureSet2Controller {
 
  
 	public static void updateMember(String email, String newPassword, String newName,
-			String newEmergencyContact, int newNrWeeks, boolean newGuideRequired,
-			boolean newHotelRequired, List<String> newItemNames, List<Integer> newItemQuantities) throws InvalidInputException {
+		String newEmergencyContact, int newNrWeeks, boolean newGuideRequired,
+		boolean newHotelRequired, List<String> newItemNames, List<Integer> newItemQuantities) throws InvalidInputException {
+
+			var error = "";
+
+			if (newPassword.isEmpty()) {
+				error = "The password cannot be empty";
+				throw new InvalidInputException(error);
+			}
+			
+			//empty name
+			if (newName.isEmpty()) {
+				error = "The name cannot be empty";
+				throw new InvalidInputException(error);
+			}
+
+			//number of climbing weeks check
+			if (newNrWeeks<=0) {
+				error = "The number of weeks must be greater than zero and less than or equal to the number of climbing weeks in the climbing season";
+				throw new InvalidInputException(error);
+			}
+		
+			if (newNrWeeks>=climbSafe.getNrWeeks()) {
+				error = "The number of weeks must be greater than zero and less than or equal to the number of climbing weeks in the climbing season";
+				throw new InvalidInputException(error);
+			}
+
+			//requested item not found
+			if (Utility.bookableItemsExists(climbSafe, newItemNames)) {
+				error = "Requested item not found";
+				throw new InvalidInputException(error);
+			}
+
+			//email already in use
+			if (Utility.findMember(email)==null) {
+				error = "A member with this email already exists";
+				throw new InvalidInputException(error);
+			}
+
+
+
+			try {
+				// Member myMember;
+				// myMember=climbSafe.addMember(email, password, name, emergencyContact, nrWeeks, guideRequired, hotelRequired);
+				// //add items too!!
+				// Utility.addItemList(climbSafe, itemNames, itemQuantities, myMember);
+				
+			}catch (RuntimeException e) {
+				error = e.getMessage();
+				throw new InvalidInputException(error);
+			}
+
+				
+
+
 		
 	}
 
