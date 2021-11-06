5958c963262aa9595c68cf66754bce3cc0e0f376
Mihail C <calitoiumihail@gmail.com>
2021-11-05 20:14:30 -0400

    wrote assignMembers method & seems logically correct

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/model/ClimbSafe.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/model/ClimbSafe.java
@@ -5,6 +5,8 @@ package ca.mcgill.ecse.climbsafe.model;
 import java.sql.Date;
 import java.util.*;
 
+import ca.mcgill.ecse.climbsafe.controller.Utility;
+
 // line 3 "../../../../../ClimbSafe.ump"
 public class ClimbSafe
 {
@@ -33,6 +35,8 @@ public class ClimbSafe
   // CONSTRUCTOR
   //------------------------
 
+ 
+  
   public ClimbSafe(Date aStartDate, int aNrWeeks, int aPriceOfGuidePerWeek)
   {
     startDate = aStartDate;
@@ -1012,6 +1016,59 @@ public class ClimbSafe
     
   }
 
+  /*
+  
+		Starting with the first guide registered in the application, go through each member starting with the first
+		member registered in the application. 
+		
+		If the member requested a guide and the guide is available for the number of weeks requested by the member, assign the guide
+		to the member at the earliest weeks for which the guide is available.
+		
+		Then continue with the next member until the guide does not have any availability anymore, at which point the process moves
+		on to the next guide (and again starts with the first member that has not yet been assigned).
+		
+		If a member did not request a guide, the member is always
+		assigned to the earliest weeks in the climbing season.
+		
+   */
+  public void assignMembers() {
+	  
+	  var climbInstance = Utility.climbSafe;
+	  List<Member> unassignedMembers = members;
+	  
+	  for (Guide currGuide: guides) {
+		  Boolean[] schedule = new Boolean[climbInstance.getNrWeeks()];
+		  for (Member currentMember: unassignedMembers) {
+			  if (currentMember == null) {continue;};
+			  if (currentMember.getGuideRequired() == false) {
+				  Assignment assignmentForMember = new Assignment(1, currentMember.getNrWeeks(), currentMember, climbInstance);
+				  if (climbInstance.addAssignment(assignmentForMember) == false) {System.out.println("error / cant add assignment");}
+			  }			
+			  
+			  int memberStayWeeks = currentMember.getNrWeeks();
+			  int startDate, endDate = 0;
+			  for (int a = 0; a < (climbInstance.getNrWeeks()-memberStayWeeks); a++) {
+				  
+				  boolean isRoom = true;
+				  for (int b = a; b<memberStayWeeks; b++) {
+					  if (schedule[b] == false) {isRoom = false;}
+
+				  }
+				  if (isRoom == true) {
+					  startDate = a;
+					  endDate = a + memberStayWeeks;
+					  Assignment assignmentForMember = new Assignment(startDate, endDate, currentMember, climbInstance);
+					  if (climbInstance.addAssignment(assignmentForMember) == false) {System.out.println("error / cant add assignment");}
+					  currentMember.getAssignment().setGuide(currGuide);
+					  currentMember = null;
+					  break;
+				  }  
+			  }
+
+		  }
+			  
+	  }
+  }
 
   public String toString()
   {
