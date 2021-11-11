eb3b7e5bca9594b3339d20d8c1556cbf8d4306db
Mihail C <68878155+MihailOfficial@users.noreply.github.com>
2021-11-10 22:44:00 -0500

    formatted code

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
@@ -13,27 +13,31 @@ import ca.mcgill.ecse.climbsafe.model.Member;
 
 public class AssignmentController {
 
- /** Initiates the assignment instances by assigning assignments to members 
-   * @author Ke Yan
-   * @author Selina Gao
-   * @author Mihail Calitoiu
-   * @throws InvalidInputException for any invalid input
-   */
+    /** Initiates the assignment instances by assigning assignments to members 
+     * @author Ke Yan
+     * @author Selina Gao
+     * @author Mihail Calitoiu
+     * @throws InvalidInputException for any invalid input
+     */
     public static void initiateAssignmentProcess() throws InvalidInputException {
 
         List < Member > forShallow = Utility.climbSafe.getMembers();
-        List < Member > unassignedMembers = new ArrayList < Member >();
-        
+        List < Member > unassignedMembers = new ArrayList < Member > ();
+
         // shallow copy of climbSafe's member array (we don't want to edit the original array)
-        for (Member temp: forShallow) { unassignedMembers.add(temp); }
-        
+        for (Member temp: forShallow) {
+            unassignedMembers.add(temp);
+        }
+
         for (Guide currentGuide: Utility.climbSafe.getGuides()) {
-        	// boolean list stores the availability per week of a specific guide (for a week, false -> available, true -> unavailable)
+            // boolean list stores the availability per week of a specific guide (for a week, false -> available, true -> unavailable)
             boolean[] schedule = new boolean[Utility.climbSafe.getNrWeeks()];
             // visit each unassigned member
             for (Member currentMember: unassignedMembers) {
-            	// already assigned, go to next member
-                if (currentMember.getName().equals("assigned")) { continue; }; 
+                // already assigned, go to next member
+                if (currentMember.getName().equals("assigned")) {
+                    continue;
+                };
                 // no guide required
                 if (currentMember.getGuideRequired() == false) {
                     Assignment assignmentForMember = new Assignment(1, currentMember.getNrWeeks(), currentMember, Utility.climbSafe);
@@ -41,29 +45,32 @@ public class AssignmentController {
                     Utility.climbSafe.addAssignment(assignmentForMember);
                     ClimbsafePersistence.save();
                     currentMember.setName("assigned");
-                    
-                // guide is required
+
+                    // guide is required
                 } else {
                     int memberStayWeeks = currentMember.getNrWeeks();
                     int startDate, endDate = 0;
                     // code below finds earliest available consecutive weeks (number being the member's stayed weeks)
-                    for (int a = 0; a < (Utility.climbSafe.getNrWeeks()-memberStayWeeks+1); a++) {
+                    for (int a = 0; a < (Utility.climbSafe.getNrWeeks() - memberStayWeeks + 1); a++) {
                         boolean isRoom = true;
                         for (int b = a; b < memberStayWeeks + a; b++) {
-                            if (schedule[b] == true) { isRoom = false; break; }
+                            if (schedule[b] == true) {
+                                isRoom = false;
+                                break;
+                            }
                         }
                         if (isRoom) {
                             startDate = a + 1;
                             endDate = a + memberStayWeeks;
                             for (int z = a; z < memberStayWeeks + a; z++) {
                                 schedule[z] = true;
-                        }
-                        Assignment assignmentForMember = new Assignment(startDate, endDate, currentMember, Utility.climbSafe);
-                        assignmentForMember.setGuide(currentGuide);
-                        Utility.climbSafe.addAssignment(assignmentForMember);
-                        ClimbsafePersistence.save();
-                        currentMember.setName("assigned");
-                        break;
+                            }
+                            Assignment assignmentForMember = new Assignment(startDate, endDate, currentMember, Utility.climbSafe);
+                            assignmentForMember.setGuide(currentGuide);
+                            Utility.climbSafe.addAssignment(assignmentForMember);
+                            ClimbsafePersistence.save();
+                            currentMember.setName("assigned");
+                            break;
                         }
                     }
                 }
@@ -78,31 +85,31 @@ public class AssignmentController {
     }
 
     /** Pays for the trip by checking whether authorizationCode is valid
-   * @author Maya Ajji
-   * @author Joey Liu
-   * @param memberEmail The email of the member
-   * @param authorizationCode The authorization code
-   * @throws InvalidInputException for any invalid input
-   */
+     * @author Maya Ajji
+     * @author Joey Liu
+     * @param memberEmail The email of the member
+     * @param authorizationCode The authorization code
+     * @throws InvalidInputException for any invalid input
+     */
     public static void payTrip(String memberEmail, String authorizationCode) throws InvalidInputException {
-        
-        
+
+
         if (Member.getWithEmail(memberEmail) == null) {
             throw new InvalidInputException("Member with email address " + memberEmail + " does not exist");
         }
-        
-        
+
+
         Member member = (Member) Member.getWithEmail(memberEmail);
         Assignment assignment = member.getAssignment();
         var error = "";
-     
+
 
         try {
-        	assignment.setAuthorizationCode(authorizationCode);
+            assignment.setAuthorizationCode(authorizationCode);
             assignment.paidForTrip();
             error = assignment.getError();
-            if(error != null) {
-            	throw new InvalidInputException(error);
+            if (error != null) {
+                throw new InvalidInputException(error);
             }
             ClimbsafePersistence.save();
         } catch (RuntimeException e) {
@@ -112,27 +119,27 @@ public class AssignmentController {
     }
 
     /** Cancels trip
-   * @author Maya Ajji
-   * @author Mihail Calitoiu
-   * @param memberEmail The email of the member
-   * @throws InvalidInputException for any invalid input
-   */
+     * @author Maya Ajji
+     * @author Mihail Calitoiu
+     * @param memberEmail The email of the member
+     * @throws InvalidInputException for any invalid input
+     */
     public static void cancelTrip(String memberEmail) throws InvalidInputException {
-        
+
         Member member = (Member) Member.getWithEmail(memberEmail);
-        
-        if (member == null){
+
+        if (member == null) {
             throw new InvalidInputException("Member with email address " + memberEmail + " does not exist ");
         }
         Assignment assignment = member.getAssignment();
         var error = "";
- 
-        
+
+
         try {
             member.getAssignment().cancelTrip();
             error = assignment.getError();
-            if(error != null) {
-            	throw new InvalidInputException(error);
+            if (error != null) {
+                throw new InvalidInputException(error);
             }
             ClimbsafePersistence.save();
         } catch (RuntimeException e) {
@@ -140,29 +147,28 @@ public class AssignmentController {
             throw new InvalidInputException(error);
         }
     }
-    
+
     /** Finishes trip
-   * @author Selina Gao
-   * @author Aigiarn Cheuk
-   * @author Joey Liu 
-   * @param memberEmail The email of the member
-   * @throws InvalidInputException for any invalid input
-   */
+     * @author Selina Gao
+     * @author Aigiarn Cheuk
+     * @author Joey Liu 
+     * @param memberEmail The email of the member
+     * @throws InvalidInputException for any invalid input
+     */
     public static void finishTrip(String memberEmail) throws InvalidInputException {
         Member member = (Member) Member.getWithEmail(memberEmail);
-        
         if (member == null) {
             throw new InvalidInputException("Member with email address " + memberEmail + " does not exist ");
         }
-        
+
         Assignment assignment = member.getAssignment();
         var error = "";
-        
+
         try {
             member.getAssignment().finishTrip();
             error = assignment.getError();
-            if(error != null) {
-            	throw new InvalidInputException(error);
+            if (error != null) {
+                throw new InvalidInputException(error);
             }
             ClimbsafePersistence.save();
         } catch (RuntimeException e) {
@@ -172,30 +178,29 @@ public class AssignmentController {
     }
 
     /** Starts the trip
-   * @author Aigiarn Cheuk
-   * @author Ke Yan
-   * @param weekNr The number of weeks
-   * @throws InvalidInputException for any invalid input
-   */
+     * @author Aigiarn Cheuk
+     * @author Ke Yan
+     * @param weekNr The number of weeks
+     * @throws InvalidInputException for any invalid input
+     */
     public static void startTrips(int weekNr) throws InvalidInputException {
         List < Assignment > assignmentInSystem = Utility.climbSafe.getAssignments();
         for (Assignment a: assignmentInSystem) {
-        	if(weekNr == a.getStartWeek()) {
-        		var error = "";               
+            if (weekNr == a.getStartWeek()) {
+                var error = "";
                 try {
-                	
+
                     a.startTrip();
                     error = a.getError();
-                    if(error != null) {
-                    	throw new InvalidInputException(error);
+                    if (error != null) {
+                        throw new InvalidInputException(error);
                     }
                     ClimbsafePersistence.save();
                 } catch (RuntimeException e) {
-                	error = e.getMessage();
+                    error = e.getMessage();
                     throw new InvalidInputException(error);
                 }
             }
         }
     }
 }
-
