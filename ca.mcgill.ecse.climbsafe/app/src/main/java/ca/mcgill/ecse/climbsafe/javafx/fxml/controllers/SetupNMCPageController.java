49d62e5a20bb22e9bec153418e8781abcb285662
J03YL1u <lucas.liuliongwah@mail.mcgill.ca>
2021-11-22 21:05:05 -0500

    setup controller

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/javafx/fxml/controllers/SetupNMCPageController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/javafx/fxml/controllers/SetupNMCPageController.java
@@ -1,5 +1,11 @@
 package ca.mcgill.ecse.climbsafe.javafx.fxml.controllers;
 
+import java.sql.Date;
+
+import ca.mcgill.ecse.climbsafe.controller.ClimbSafeFeatureSet1Controller;
+
+import static ca.mcgill.ecse.climbsafe.javafx.fxml.controllers.ViewUtils.successful;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -21,7 +27,36 @@ public class SetupNMCPageController {
 	  private Button startSeasonButton;
 	  
 	  
-	  public void startSeasonClicked() {
+	  public void startSeasonClicked(ActionEvent event) {
+		  String adminEmail = this.adminEmailTextField.getText();
+		  if (adminEmail == null || adminEmail.trim().isEmpty()) {
+		      ViewUtils.showError("Please input a valid email address");
+		  }
+		  String password = this.passwordTextField.getText();
+		  if (password == null || password.trim().isEmpty()) {
+		      ViewUtils.showError("Please input a valid password");
+		  }
+		  String pricesOfGuide = this.priceTextField.getText();
+		  if (pricesOfGuide == null || pricesOfGuide.trim().isEmpty()) {
+		      ViewUtils.showError("Please input a valid price for the guides");
+		  }
 		  
+		  try {
+			  
+			  int priceGuide = Integer.parseInt(pricesOfGuide);
+			  int nrOfWeeks = Integer.parseInt(this.endDateTextField.getText());
+			  Date startDate = Date.valueOf(this.startDateTextField.getText());
+			  
+			  
+			  if (successful(() -> ClimbSafeFeatureSet1Controller.setup(startDate, nrOfWeeks, priceGuide))) {
+			        this.adminEmailTextField.setText("");
+			        this.startDateTextField.setText("");
+			        this.passwordTextField.setText("");
+			        this.endDateTextField.setText("");
+			        this.priceTextField.setText("");
+			  }
+			  
+		  }catch (RuntimeException e) {
+		  }
 	  }
 }
