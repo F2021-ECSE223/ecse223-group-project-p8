bc2019b28d564c012d2397945871bbf4822008f0
J03YL1u <lucas.liuliongwah@mail.mcgill.ca>
2021-11-24 15:33:40 -0500

    commit

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/javafx/fxml/controllers/SetupNMCPageController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/javafx/fxml/controllers/SetupNMCPageController.java
@@ -1,33 +1,33 @@
 package ca.mcgill.ecse.climbsafe.javafx.fxml.controllers;
 
+import static ca.mcgill.ecse.climbsafe.javafx.fxml.controllers.ViewUtils.successful;
+
 import java.sql.Date;
 
 import ca.mcgill.ecse.climbsafe.controller.ClimbSafeFeatureSet1Controller;
-
-import static ca.mcgill.ecse.climbsafe.javafx.fxml.controllers.ViewUtils.successful;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+
 import javafx.scene.control.Button;
+
 import javafx.scene.control.TextField;
 
 public class SetupNMCPageController {
+	@FXML
+	private TextField adminEmailTextField;
+	@FXML
+	private TextField passwordTextField;
+	@FXML
+	private TextField startDateTextField;
+	@FXML
+	private TextField nrOfWeeksTextField;
+	@FXML
+	private TextField priceTextField;
+	@FXML
+	private Button startSeasonButton;
 	
-	  @FXML
-	  private TextField adminEmailTextField;
-	  @FXML
-	  private TextField passwordTextField;
-	  @FXML
-	  private TextField startDateTextField;
-	  @FXML
-	  private TextField nrOfWeeksTextField;
-	  @FXML
-	  private TextField priceTextField;
-	  @FXML
-	  private Button startSeasonButton;
-	  
-	  
-	  public void startSeasonClicked(ActionEvent event) {
+	
+	public void startSeasonClicked(ActionEvent event) {
 		  String adminEmail = this.adminEmailTextField.getText();
 		  if (adminEmail == null || adminEmail.trim().isEmpty()) {
 		      ViewUtils.showError("Please input a valid email address");
