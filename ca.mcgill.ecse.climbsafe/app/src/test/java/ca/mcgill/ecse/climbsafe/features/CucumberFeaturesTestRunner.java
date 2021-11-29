651d9f9a279f0ffa8c8295f7795bf77a820a29b4
J03YL1u <lucas.liuliongwah@mail.mcgill.ca>
2021-11-29 11:30:13 -0500

    ui setup update

--- ca.mcgill.ecse.climbsafe/app/src/test/java/ca/mcgill/ecse/climbsafe/features/CucumberFeaturesTestRunner.java
+++ ca.mcgill.ecse.climbsafe/app/src/test/java/ca/mcgill/ecse/climbsafe/features/CucumberFeaturesTestRunner.java
@@ -6,6 +6,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(plugin = "pretty", features = "src/test/resources")
+@CucumberOptions(plugin = "pretty", features = "src/test/resources/RegisterGuide.feature")
 public class CucumberFeaturesTestRunner {
 }
