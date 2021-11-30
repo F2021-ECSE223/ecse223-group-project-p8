3acae1464ceaeab98c7c6b8f64339e8bf70310df
aigiarnc <aigiarn.cheuk@gmail.com>
2021-11-30 17:12:08 -0500

    almost there:D

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/javafx/fxml/controllers/equipmentBundlePageController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/javafx/fxml/controllers/equipmentBundlePageController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 
 import static ca.mcgill.ecse.climbsafe.javafx.fxml.controllers.ViewUtils.successful;
 
@@ -12,7 +14,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ca.mcgill.ecse.climbsafe.controller.ClimbSafeFeatureSet5Controller;
+import ca.mcgill.ecse.climbsafe.controller.ClimbSafeFeatureSet6Controller;
 import ca.mcgill.ecse.climbsafe.controller.InvalidInputException;
+import ca.mcgill.ecse.climbsafe.controller.TOAssignment;
 import ca.mcgill.ecse.climbsafe.controller.Utility;
 import ca.mcgill.ecse.climbsafe.javafx.fxml.ClimbsafeFxmlView;
 import ca.mcgill.ecse.climbsafe.model.BookedItem;
@@ -25,7 +29,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 
 import javafx.scene.control.Tab;
-
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableView;
 
 import javafx.scene.control.ChoiceBox;
@@ -35,8 +39,8 @@ import javafx.scene.control.TableColumn;
 public class equipmentBundlePageController {
 
 	// Storing Items
-	ArrayList<Equipment> equipmentsCB = new ArrayList<Equipment>();
-	ArrayList<Equipment> equipmentsEB = new ArrayList<Equipment>();
+	ObservableList<Equipment> equipmentsCB = FXCollections.observableArrayList();
+	ObservableList<Equipment> equipmentsEB = FXCollections.observableArrayList();
 	ArrayList<String> equipmentNamesCB = new ArrayList<String>();
 	ArrayList<String> equipmentNamesEB = new ArrayList<String>();
 	ArrayList<Integer> equipmentQuantitiesCB = new ArrayList<Integer>();
@@ -46,6 +50,8 @@ public class equipmentBundlePageController {
 	private TextField bundleNameInput;
 	@FXML
 	private TableView<Equipment> bundleItemsTable;
+	@FXML
+	private TableView<Equipment> bundleNewItemsTable;
 	@FXML
 	private TableColumn<Equipment, String> bundleEquipmentCB;
 	@FXML
@@ -88,6 +94,12 @@ public class equipmentBundlePageController {
 	private Button updateEquipmentBundleButton;
 	@FXML
 	private TextField guideDiscountEB;
+	@FXML
+	private Button clearEquipmentCBButton;
+	@FXML
+	private Button clearEquipmentEBButton;
+	@FXML
+	private Button deleteBundleButton;
 
 	@FXML
 	public void initialize() {
@@ -112,6 +124,57 @@ public class equipmentBundlePageController {
 
 		ClimbsafeFxmlView.getInstance().registerRefreshEvent(equipmentMenuCB, selectBundleMenu, equipmentMenuEB);
 
+		// initialize bundleItemsTable
+		bundleEquipmentCB.setCellValueFactory(new PropertyValueFactory<Equipment, String>("name"));
+		costCB.setCellValueFactory(new PropertyValueFactory<Equipment, Double>("pricePerWeek"));
+		weightCB.setCellValueFactory(new PropertyValueFactory<Equipment, Double>("weight"));
+
+		bundleItemsTable.addEventHandler(ClimbsafeFxmlView.REFRESH_EVENT, e -> bundleItemsTable.setItems(equipmentsCB));
+
+		ClimbsafeFxmlView.getInstance().registerRefreshEvent(bundleItemsTable);
+		
+		//initialize bundleNewItemsTable
+		bundleEquipmentEB.setCellValueFactory(new PropertyValueFactory<Equipment, String>("name"));
+		costEB.setCellValueFactory(new PropertyValueFactory<Equipment, Double>("pricePerWeek"));
+		weightEB.setCellValueFactory(new PropertyValueFactory<Equipment, Double>("weight"));
+
+		bundleNewItemsTable.addEventHandler(ClimbsafeFxmlView.REFRESH_EVENT, e -> bundleNewItemsTable.setItems(equipmentsEB));
+
+		ClimbsafeFxmlView.getInstance().registerRefreshEvent(bundleNewItemsTable);
+	}
+
+	// Event Listener on Button[#clearEquipmentCBclicked].onAction
+	@FXML
+	public void clearEquipmentCBclicked(ActionEvent event) {
+		bundleItemsTable.getItems().clear();
+		equipmentsCB.clear();
+		equipmentNamesCB.clear();
+		equipmentQuantitiesCB.clear();
+	}
+
+	// Event Listener on Button[#clearEquipmentEBclicked].onAction
+	@FXML
+	public void clearEquipmentEBclicked(ActionEvent event) {
+		bundleNewItemsTable.getItems().clear();
+		equipmentsEB.clear();
+		equipmentNamesEB.clear();
+		equipmentQuantitiesEB.clear();
+	}
+
+	// Event Listener on Button[#deleteBundle].onAction
+	@FXML
+	public void deleteBundle(ActionEvent event) {
+		String equipmentName = selectBundleMenu.getValue();
+		if (equipmentName == null) {
+			ViewUtils.showError("Please select an equipment before deleting it");
+		} else {
+				if (successful(() -> ClimbSafeFeatureSet6Controller.deleteEquipmentBundle(equipmentName))) {
+					this.selectBundleMenu.setValue(null);
+					this.newBundleNameInput.setText("");
+					this.guideDiscountEB.setText("");
+					ClimbsafeFxmlView.getInstance().refresh();
+				}
+		}
 	}
 
 	// Event Listener on Button[#addEquipmentToBundleButton].onAction
@@ -131,6 +194,7 @@ public class equipmentBundlePageController {
 					equipmentQuantitiesCB.add(quantity);
 					equipmentMenuCB.setValue(null);
 					equipmentQuantityInputCB.setText("");
+					ClimbsafeFxmlView.getInstance().refresh();
 				}
 			} catch (NumberFormatException e) {
 				ViewUtils.showError("Please input a quantity");
@@ -154,7 +218,11 @@ public class equipmentBundlePageController {
 						this.equipmentNamesCB, this.equipmentQuantitiesCB))) {
 					this.bundleNameInput.setText("");
 					this.guideDiscountCB.setText("");
-					// update tables
+					bundleItemsTable.getItems().clear();
+					equipmentsCB.clear();
+					equipmentNamesCB.clear();
+					equipmentQuantitiesCB.clear();
+					ClimbsafeFxmlView.getInstance().refresh();
 				}
 			} catch (NumberFormatException e) {
 				ViewUtils.showError("Please input a valid discount number");
@@ -176,6 +244,7 @@ public class equipmentBundlePageController {
 				equipmentQuantitiesEB.add(quantity);
 				equipmentMenuEB.setValue(null);
 				equipmentQuantityInputEB.setText("");
+				ClimbsafeFxmlView.getInstance().refresh();
 			} catch (NumberFormatException e) {
 				ViewUtils.showError("Please input a quantity before updating");
 			}
@@ -202,7 +271,11 @@ public class equipmentBundlePageController {
 					this.selectBundleMenu.setValue(null);
 					this.newBundleNameInput.setText("");
 					this.guideDiscountEB.setText("");
-					// update tables
+					bundleNewItemsTable.getItems().clear();
+					equipmentsEB.clear();
+					equipmentNamesEB.clear();
+					equipmentQuantitiesEB.clear();
+					ClimbsafeFxmlView.getInstance().refresh();
 				}
 			} catch (NumberFormatException e) {
 				ViewUtils.showError("Please input a valid discount number");
